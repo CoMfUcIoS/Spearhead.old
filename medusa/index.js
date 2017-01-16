8af247f673424df1f977804ecade7eeb90058f76
Ioannis Karasavvaidis <john@studio110.eu>
2017-01-16 15:36:32 +0000

    hardcoded certs.

--- medusa/index.js
+++ medusa/index.js
@@ -24,12 +24,30 @@ app.get('/overview', function(req, res) {
 
 var options = {
   // // Private key
-  // key: fs.readFileSync(__dirname + '/keys/spdy-key.pem'),
+  key : '/home/osmc/letsencrypt/etc/live/rpi.studio110.eu/privkey.pem',
 
   // // Fullchain file or cert file (prefer the former)
-  // cert: fs.readFileSync(__dirname + '/keys/spdy-fullchain.pem'),
+  cert : '/home/osmc/letsencrypt/etc/live/rpi.studio110.eu/fullchain.pem',
 
   // **optional** SPDY-specific options
+  // spdy : {
+  //   protocols : ['h2', 'spdy/3.1', 'http/1.1'],
+  //   plain     : false,
+
+  //   // **optional**
+  //   // Parse first incoming X_FORWARDED_FOR frame and put it to the
+  //   // headers of every request.
+  //   // NOTE: Use with care! This should not be used without some proxy that
+  //   // will *always* send X_FORWARDED_FOR
+  //   'x-forwarded-for' : true,
+
+  //   connection : {
+  //     windowSize : 1024 * 1024, // Server's window size
+
+  //     // **optional** if true - server will send 3.1 frames on 3.0 *plain* spdy
+  //     autoSpdy31 : false
+  //   }
+  // }
 };
 
 spdy
