66181c1dcc1b8049b43f498e688c62dcdc30f9f4
Ioannis Karasavvaidis <john@studio110.eu>
2017-01-16 12:55:08 +0000

    Some checks of the domain for ws !

--- hydra/index.js
+++ hydra/index.js
@@ -1,115 +1,119 @@
-  import { server as Server } from 'websocket';
-  import http                 from 'http';
-  import chimera              from '../chimera/index.js';
+import { server as Server } from 'websocket';
+import https                from 'https';
+import http                 from 'http';
+import chimera              from '../chimera/index.js';
 
 
-  const requires = [
-        'util',
-        'config'
-      ],
-      { config, util }  = chimera.initialize(requires),
-      port = config.get('ports.hydra'),
-      httpServer = http.createServer(function(request, response) {
-        util.log((new Date()) + ' Received request for ' + request.url);
-        response.writeHead(404);
-        response.end();
-      }),
+const requires = [
+      'util',
+      'config'
+    ],
+    { config, util }  = chimera.initialize(requires),
+    port = config.get('ports.hydra'),
+    httpServer = (config.get('debug')) ? http.createServer(httpserver) : https.createServer(httpserver),
 
-      wsServer = new Server({
-        httpServer,
-      // You should not use autoAcceptConnections for production
-      // applications, as it defeats all standard cross-origin protection
-      // facilities built into the protocol and the browser.  You should
-      // *always* verify the connection's origin and decide whether or not
-      // to accept it.
-        autoAcceptConnections : false
-      }),
-      CLIENTS = {};
+    wsServer = new Server({
+      httpServer,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+      autoAcceptConnections : false
+    }),
+    CLIENTS = {};
 
-  httpServer.listen(port, function() {
-    util.log(`Hydra server is listening on port ${port}`);
-  });
+httpServer.listen(port, function() {
+  util.log(`Hydra server is listening on port ${port}`);
+});
 
 
-  function originIsAllowed(origin) {
-    // accept our apps.
-    let allowed = _itsOurOwnApp(origin) && (!CLIENTS[origin]);
+function httpserver(request, response) {
+  util.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
+}
 
-    if (!allowed && util.toType(origin) === 'string') {
-      allowed = (origin.indexOf(`${util.hostname().toLowerCase()}.local`) > -1);
-    }
+function originIsAllowed(origin) {
+  // accept our apps.
+  let allowed = _itsOurOwnApp(origin) && (!CLIENTS[origin]);
 
-    return allowed;
+  if (!allowed && util.toType(origin) === 'string') {
+    allowed = (origin.indexOf(`${util.hostname().toLowerCase()}.local`) > -1);
   }
 
-  function _itsOurOwnApp(app) {
-    const allowedOrigins = Object.keys(config.get('ports')),
-        ownApp = (allowedOrigins.indexOf(app) > -1);
-    return ownApp;
+  return allowed;
+}
+
+function _itsOurOwnApp(app) {
+  const allowedOrigins = Object.keys(config.get('ports')),
+      domain = app.match(/\w+\.\w+[.\w+]+/g);
+  return ((allowedOrigins.indexOf(app) > -1) || (allowedOrigins.indexOf(domain[0]) > -1));
+}
+
+
+wsServer.on('request', function(request) {
+  console.log(request);
+  const origin = request.origin,
+      ownApp = _itsOurOwnApp(origin),
+      uuid = util.uuid();
+
+  if (!originIsAllowed(origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    util.log(' Connection from origin ' + origin + ' rejected.');
+    return;
   }
 
+  let connection = request.accept('echo-protocol', origin);
+  util.log(`Connection from ${origin} accepted. Client Id ${uuid}`);
 
-  wsServer.on('request', function(request) {
-    const origin = request.origin,
-        ownApp = _itsOurOwnApp(origin),
-        uuid = util.uuid();
-
-    if (!originIsAllowed(origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      util.log(' Connection from origin ' + origin + ' rejected.');
-      return;
+  if (ownApp) {
+    CLIENTS[origin] = connection;
+  } else {
+    if (util.toType(CLIENTS[origin]) !== 'object') {
+      CLIENTS[origin] = {};
     }
+    CLIENTS[origin][uuid] = connection;
+  }
 
-    let connection = request.accept('echo-protocol', origin);
-    util.log(`Connection from ${origin} accepted. Client Id ${uuid}`);
-
-    if (ownApp) {
-      CLIENTS[origin] = connection;
-    } else {
-      if (util.toType(CLIENTS[origin]) !== 'object') {
-        CLIENTS[origin] = {};
-      }
-      CLIENTS[origin][uuid] = connection;
-    }
-
-    connection.on('message', function(message) {
-      message = JSON.parse(message.utf8Data);
-      if (util.object.get(message, 'to')) {
-        let connectionTo;
-        if (message.to !== 'all') {
-          if (_itsOurOwnApp(message.to)) {
-            connectionTo = util.object.get(CLIENTS, message.to);
-          } else {
-            connectionTo = util.object.get(CLIENTS[origin], message.to);
-          }
-
-          if (connectionTo) {
-            connectionTo.send(JSON.stringify(message.message));
-          }
+  connection.on('message', function(message) {
+    message = JSON.parse(message.utf8Data);
+    if (util.object.get(message, 'to')) {
+      let connectionTo;
+      if (message.to !== 'all') {
+        if (_itsOurOwnApp(message.to)) {
+          connectionTo = util.object.get(CLIENTS, message.to);
         } else {
-          util.log('To ALl ... but who ? ownApps or not ?');
+          connectionTo = util.object.get(CLIENTS[origin], message.to);
         }
-      } else if (util.toType(message) !== 'object') {
-        util.log('What i got smth without a type? Huh ?');
+
+        if (connectionTo) {
+          connectionTo.send(JSON.stringify(message.message));
+        }
       } else {
-        switch (message.type) {
-          case 'message' : {
-            util.log(`Got a mesage from ${origin} - ${uuid}, message : ${message.message}`);
-            break;
-          }
+        util.log('To ALl ... but who ? ownApps or not ?');
+      }
+    } else if (util.toType(message) !== 'object') {
+      util.log('What i got smth without a type? Huh ?');
+    } else {
+      switch (message.type) {
+        case 'message' : {
+          util.log(`Got a mesage from ${origin} - ${uuid}, message : ${message.message}`);
+          break;
         }
       }
-    });
-
-    connection.send(JSON.stringify({ type : 'uuid', uuid }));
-
-    connection.on('close', function(reasonCode, description) {
-      if (!ownApp) {
-        delete CLIENTS[origin][uuid];
-      } else {
-        delete CLIENTS[origin];
-      }
-      util.log(`Peer ${connection.remoteAddress} with uuid : ${uuid} disconnected.`);
-    });
+    }
   });
+
+  connection.send(JSON.stringify({ type : 'uuid', uuid }));
+
+  connection.on('close', function(reasonCode, description) {
+    if (!ownApp) {
+      delete CLIENTS[origin][uuid];
+    } else {
+      delete CLIENTS[origin];
+    }
+    util.log(`Peer ${connection.remoteAddress} with uuid : ${uuid} disconnected.`);
+  });
+});
