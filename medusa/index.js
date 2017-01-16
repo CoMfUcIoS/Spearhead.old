fa1500f934bd4644159e1fdaaf343772d1db046c
Ioannis Karasavvaidis <john@studio110.eu>
2017-01-16 15:32:37 +0000

    No options passed to spdy

--- medusa/index.js
+++ medusa/index.js
@@ -30,24 +30,6 @@ var options = {
   // cert: fs.readFileSync(__dirname + '/keys/spdy-fullchain.pem'),
 
   // **optional** SPDY-specific options
-  spdy : {
-    protocols : ['h2', 'spdy/3.1', 'http/1.1'],
-    plain     : false,
-
-    // **optional**
-    // Parse first incoming X_FORWARDED_FOR frame and put it to the
-    // headers of every request.
-    // NOTE: Use with care! This should not be used without some proxy that
-    // will *always* send X_FORWARDED_FOR
-    'x-forwarded-for' : true,
-
-    connection : {
-      windowSize : 1024 * 1024, // Server's window size
-
-      // **optional** if true - server will send 3.1 frames on 3.0 *plain* spdy
-      autoSpdy31 : false
-    }
-  }
 };
 
 spdy
