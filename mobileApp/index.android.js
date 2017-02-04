2d711119137c6f09c98470b62bfe5d8512c3e5e9
Ioannis Karasavvaidis <john@studio110.eu>
2017-02-04 20:58:10 +0000

    Some tidy up

--- mobileApp/index.android.js
+++ mobileApp/index.android.js
@@ -32,21 +32,21 @@ export default class AwesomeProject extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  container : {
+    flex            : 1,
+    justifyContent  : 'center',
+    alignItems      : 'center',
+    backgroundColor : '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  welcome : {
+    fontSize  : 20,
+    textAlign : 'center',
+    margin    : 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  instructions : {
+    textAlign    : 'center',
+    color        : '#333333',
+    marginBottom : 5,
   },
 });
 
